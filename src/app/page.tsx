5ea32df6d12dca8645b62e2dd5aa9d12e8b4dea7
J33t <j33tj3@gmail.com>
2025-01-27 21:18:07 +0100

    Create poll button at poll list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
 import PollsList from "@/components/PollsList";
-import { Card } from "@mui/material";
+import { Button, Card } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <Card
-      sx={{
-        maxWidth: { xs: "100%", md: "80%" },
-        bgcolor: "background.default",
-        margin: "auto",
-        marginTop: { xs: 2, md: 4 },
-        marginBottom: { xs: 2, md: 4 },
-        boxShadow: { xs: 0, md: 2 },
-      }}
-    >
-      <PollsList />
-    </Card>
+    <>
+      <Card
+        sx={{
+          maxWidth: { xs: "100%", md: "80%" },
+          bgcolor: "background.default",
+          margin: "auto",
+          marginTop: { xs: 2, md: 4 },
+          marginBottom: { xs: 2, md: 4 },
+          boxShadow: { xs: 0, md: 2 },
+        }}
+      >
+        <PollsList />
+      </Card>
+      <Button
+        variant="contained"
+        sx={{ marginX: "auto", maxWidth: { xs: "100%", md: "80%" } }}
+        href="/create-poll"
+      >
+        Maak poll aan
+      </Button>
+    </>
   );
 }
