581d2462f826edbb4fcd88b023b544c4dc0bb2a5
J33t <j33tj3@gmail.com>
2025-01-28 22:12:59 +0100

    Add copy to clipboard feature for sharing purpose

--- src/components/PollsList/index.tsx
+++ src/components/PollsList/index.tsx
@@ -1,56 +1,142 @@
 "use client";
 
-import { Poll, usePolls } from "@/utils/usePolls";
+import { Poll, Polls } from "@/utils/usePolls";
+import { baseUrl } from "@/utils/utils";
+import { AccessTime, Check, ContentCopy } from "@mui/icons-material";
+
 import {
+  Alert,
   Box,
+  Button,
+  Card,
+  Fade,
   Table,
   TableBody,
   TableCell,
-  TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { LoadingSpinner } from "../layout/LoadingSpinner";
+import { useEffect, useState } from "react";
 
-const PollsList = () => {
+const PollsList = ({ data }: { data: Polls }) => {
   const router = useRouter();
-  const { data, isLoading, error } = usePolls();
-
-  if (isLoading) return <LoadingSpinner />;
-  if (error) return <p>Error: {error.message}</p>;
-
+  const [alert, setAlert] = useState(false);
   const { result } = data;
+  const desktopOnly = { display: { xs: "none", md: "table-cell" } };
 
-  const desktopOnly = { display: { xs: "none", sm: "table-cell" } };
+  useEffect(() => {
+    if (alert) {
+      const timer = setTimeout(() => {
+        setAlert(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [alert]);
 
   return (
-    <Box component="div" className="relative  w-full h-full">
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell sx={desktopOnly}>Time</TableCell>
-            <TableCell sx={desktopOnly}>Trainer</TableCell>
-            <TableCell sx={desktopOnly}>Location</TableCell>
-            <TableCell>Title</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {result.map((poll: Poll) => (
-            <TableRow
-              key={poll.id}
-              onClick={() => router.push(`/${poll.id}`)}
-              className="cursor-pointer hover:bg-blue-100"
+    <Box
+      component="div"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        maxWidth: { xs: "100%", md: "80%" },
+      }}
+    >
+      {alert && (
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            left: "50%",
+            transform: "translateX(-50%)",
+            padding: { xs: 2, md: 4 },
+            zIndex: 1000,
+          }}
+        >
+          <Fade in={alert}>
+            <Alert
+              icon={<Check fontSize="inherit" />}
+              severity="success"
+              sx={{
+                whiteSpace: "nowrap",
+                width: { xs: "100%", md: "80%" },
+              }}
             >
-              <TableCell>{poll.date}</TableCell>
-              <TableCell sx={desktopOnly}>{poll.time}</TableCell>
-              <TableCell sx={desktopOnly}>{poll.trainer}</TableCell>
-              <TableCell sx={desktopOnly}>{poll.location}</TableCell>
-              <TableCell>{poll.title}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+              Tekst is gekopieerd naar het klembord!
+            </Alert>
+          </Fade>
+        </Box>
+      )}
+
+      <Card
+        sx={{
+          bgcolor: "background.default",
+          boxShadow: { xs: 0, md: 2 },
+        }}
+      >
+        <Table>
+          <TableBody>
+            {result.map((poll: Poll) => {
+              const handleCopy = () => {
+                const text = `Doe je mee met "${poll.title}"\nDatum: ${poll.date}\nTijd: ${poll.time}\nLocatie: ${poll.location}\nTrainer: ${poll.trainer}\n\n${baseUrl}/${poll.id}`;
+
+                navigator.clipboard
+                  .writeText(text)
+                  .then(() => setAlert(true))
+                  .catch((err) => console.error("Error copying text: ", err));
+              };
+              return (
+                <TableRow
+                  key={poll.id}
+                  onClick={() => router.push(`/${poll.id}`)}
+                  className="cursor-pointer hover:bg-blue-100"
+                >
+                  <TableCell>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-start",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      <Typography variant="caption">{poll.date}</Typography>
+                      <Typography
+                        variant="caption"
+                        sx={{ display: "flex", gap: 0.5, alignItems: "center" }}
+                      >
+                        <AccessTime sx={{ height: 14, width: 14 }} />
+                        {poll.time}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell sx={desktopOnly}>{poll.time}</TableCell>
+                  <TableCell sx={desktopOnly}>{poll.trainer}</TableCell>
+                  <TableCell sx={desktopOnly}>{poll.location}</TableCell>
+                  <TableCell>
+                    <Box
+                      sx={{ display: "flex", justifyContent: "space-between" }}
+                    >
+                      {poll.title}
+                      <Button
+                        sx={{ minWidth: 0, flexShrink: 0 }}
+                        onClick={(e) => {
+                          handleCopy();
+                          e.stopPropagation();
+                        }}
+                      >
+                        <ContentCopy />
+                      </Button>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Card>
     </Box>
   );
 };
